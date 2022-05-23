a5a734e009e3cfe938c950cf291d33e3d21b4561
Agamdeep Singh <agamdeep579@gmail.com>
2022-05-23 14:00:57 +0530

    Added the code

---
+++ main.py
@@ -0,0 +1,25 @@
+from ast import Break
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    query = input(f"What Would You Like? {options}: ")
+    
+    if query == "off":
+        is_on = False
+    
+    elif query == "report":
+        coffee_maker.report()
+        money_machine.report()
+    
+    else:
+        order = menu.find_drink(query)
+        if coffee_maker.is_resource_sufficient(order) and money_machine.make_payment(order.cost):
+            coffee_maker.make_coffee(order)
