b4378231b605db058c8968e2db4961330244934e
Agamdeep Singh <agamdeep579@gmail.com>
2022-06-01 01:34:47 +0530

    Updated the code

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from ast import Break
+# Created by Agamdeep Singh / CodeWithAgam
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @coderagam001 / @codewithagam
+# Twitter: @CoderAgam001
+# Linkdin: Agamdeep Singh
+
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
